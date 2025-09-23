5b56ce901879b7c403accaee76214892ca512777
UMUT <149240596+MustafaUmutIdam@users.noreply.github.com>
2025-09-23 19:25:40 +0300

    storyControler was added and connected to DB

---
+++ InstagramCloneBackend/InstagramCloneBackend/Migrations/20250923160349_AddStoryTable1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstagramCloneBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoryTable1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsMine = table.Column<bool>(type: "bit", nullable: false),
+                    HasStory = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stories");
+        }
+    }
+}
---
+++ InstagramCloneBackend/InstagramCloneBackend/Models/Story.cs
@@ -0,0 +1,11 @@
+﻿namespace InstagramCloneBackend.Models
+{
+    public class Story
+    {
+        public int Id { get; set; }  // DB otomatik atar
+        public string ProfileName { get; set; } = string.Empty;
+        public string ProfileImageUrl { get; set; } = string.Empty;
+        public bool IsMine { get; set; } = false;
+        public bool HasStory { get; set; } = true;
+    }
+}
