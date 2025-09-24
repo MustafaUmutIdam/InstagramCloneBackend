f0d7e4be7f6d61fcd668a08f3fecca923a88beb1
UMUT <149240596+MustafaUmutIdam@users.noreply.github.com>
2025-09-24 15:36:26 +0300

    Profile Model updated

--- InstagramCloneBackend/InstagramCloneBackend/Migrations/20250924123404_AddProfileImageUrl.Designer.cs
+++ InstagramCloneBackend/InstagramCloneBackend/Migrations/20250924123404_AddProfileImageUrl.Designer.cs
@@ -3,6 +3,7 @@ using InstagramCloneBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InstagramCloneBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250924123404_AddProfileImageUrl")]
+    partial class AddProfileImageUrl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
